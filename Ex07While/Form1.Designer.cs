50d054c0012e0ceda949d471128b2db4e473b729
miltonpalmeira <lpsantana_@hotmail.com>
2024-11-13 11:31:35 -0300

    Adicionar arquivos de projeto.

---
+++ Ex07While/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Ex07While
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRazao = new System.Windows.Forms.TextBox();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lsbPA = new System.Windows.Forms.ListBox();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite a razão da PA:";
+            // 
+            // txtRazao
+            // 
+            this.txtRazao.Location = new System.Drawing.Point(13, 57);
+            this.txtRazao.Name = "txtRazao";
+            this.txtRazao.Size = new System.Drawing.Size(275, 38);
+            this.txtRazao.TabIndex = 1;
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(13, 307);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(130, 67);
+            this.btnMostrar.TabIndex = 2;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lsbPA
+            // 
+            this.lsbPA.FormattingEnabled = true;
+            this.lsbPA.ItemHeight = 31;
+            this.lsbPA.Location = new System.Drawing.Point(13, 101);
+            this.lsbPA.Name = "lsbPA";
+            this.lsbPA.Size = new System.Drawing.Size(193, 190);
+            this.lsbPA.TabIndex = 3;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(266, 307);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(130, 67);
+            this.btnSair.TabIndex = 4;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 389);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.lsbPA);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtRazao);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRazao;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lsbPA;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Ex07While/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex07While
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            int razao, pa = 0;
+            int i = 0;
+            razao = int.Parse(txtRazao.Text);
+
+            while (i < 10)
+            {
+                pa = pa + razao;
+                lsbPA.Items.Add(pa);
+                i++;
+            }
+        }
+    }
+}
